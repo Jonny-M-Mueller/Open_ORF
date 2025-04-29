5df4d0d4c3d7e72adee8136f756a0324f3b7f55e
Jonny Müller <jonny.mueller@posteo.de>
2025-04-29 15:46:32 +0200

    committed non functional iteral approach for creating MarkDownfile with highligted ORFs to seperate git branch as a backup

--- python_approach/ORF_regex.py
+++ python_approach/ORF_regex.py
@@ -1,16 +1,57 @@
 #! /bin/ env python3
 import re
-INPUT_FILE=open('INPUT/HTR2A.txt')
+import itertools
+INPUT_FILE=open('INPUT/HTR2A_short.txt')
 OUTPUT_FILE=open('OUTPUT.txt', "w")
 PRE_INPUT_STRING=INPUT_FILE.read()
 INPUT_FILE.close()
+MD_FILE=open('OUTPUT.md', "w")
 INPUT_STRING=PRE_INPUT_STRING.replace("\n", "")
 
+INDEX=0 #is not a true index but is used to slice the iterable
 pattern=re.compile('(?=(ATG([CTGA]{3})*?(TGA|TAA|TAG)))')
-for ORF in pattern.finditer(INPUT_STRING):
+ORF_ITER=pattern.finditer(INPUT_STRING)  #pattern.finditer = LIST OF FOUND PATTERNS; ORF = found pattern at loop position in LIST 'pattern.finditer'
+for ORF in pattern.finditer(INPUT_STRING):  #BUG RELATED earlier problem is because itteratortools.islice CONSUMES the sliced part (i.e. the orignial iterator advances past the sliced part)
+    INDEX=INDEX+1
+    ABOVE=INDEX+1
+    BELOW=INDEX-1
     length=ORF.end(1) - ORF.start(1)
     length_trip=length//3
     PEP=""
+    START_IN_EARLIER="false"
+    END_IN_EARLIER="false"
+    END_IN_LATER="false"
+
+    EARLIER_ORFS=itertools.islice(ORF_ITER, BELOW)  #BUG PROBLEM IS LIKELY HERE!! I think i misunderstood islice ITERSLICE CREATES AN ITER FROM ELEMENTS not A SLICE FROM AN ITER
+    LATER_ORFS=itertools.islice(ORF_ITER, ABOVE, None)  #likely: cosumption of ORF_ITER in both directions prevents the function creating usefull output after the first time
+    print("Earlier_start=", EARLIER_ORFS)
+    print("later=", LATER_ORFS)
+    for e in EARLIER_ORFS:                       # BUG all if here return false
+        print("e.start=", e.start(1))
+        if e.end(1) > ORF.start(1):
+            START_IN_EARLIER="true"
+            if e.end(1) > ORF.end(1):
+                END_IN_EARLIER="true"
+                break
+
+    for l in LATER_ORFS:
+        print("l=", l)
+        if l.start(1) < ORF.end(1):
+            END_IN_LATER="true"
+            break
+    #search an replace block
+    if  START_IN_EARLIER=="false" and END_IN_LATER=="false":    # ORF all alone
+        REPLACE_STRING="\*\*" + ORF.group(1) + "\*\*"
+
+    if  START_IN_EARLIER=="true" and END_IN_LATER=="false":      # START of ORF is located within 1 or more earlier ORFS but end is alone
+        REPLACE_STRING=ORF.group(1) + "\*\*"
+    if  START_IN_EARLIER=="false" and END_IN_LATER=="true":      # END of ORF is located within 1 or more other but START is alone
+        REPLACE_STRING="\*\*" + ORF.group(1)
+    if  END_IN_EARLIER=="true":                                  # END and START of ORF is located within 1 or more other ORFS
+        REPLACE_STRING=ORF.group(1)
+    INPUT_STRING.replace(ORF.group(1), REPLACE_STRING)
+    print("Se ", START_IN_EARLIER, " Ee ", END_IN_EARLIER, " El ", END_IN_LATER)
+
     for i in range(length_trip):
         j=i*3
         k=j+3
@@ -61,11 +102,11 @@ for ORF in pattern.finditer(INPUT_STRING):
              case 'TAA' | 'TGA' | 'TAG':
                  PEP=PEP
 
-
-
     print('length',length,'   #triplets', length_trip)
     print('======================================',file=OUTPUT_FILE )
     print('START', ORF.start(1), '    END', ORF.end(1),'LENGTH', length, file=OUTPUT_FILE )
     print( ORF.group(1),file=OUTPUT_FILE)
     print(PEP,file=OUTPUT_FILE)
-   # print(ORF.group(1), ORF.span(1))
+print(INPUT_STRING, file=MD_FILE)
+OUTPUT_FILE.close()
+MD_FILE.close()
