0a35d336fe245233f14502e5f0f11b8f52708604
Jonny Müller <jonny.mueller@posteo.de>
2025-05-15 16:48:56 +0200

    ORF_regex.py can now identify TATA-Boxes and rate the strength of the Kozak Sequence

--- ORF_regex.py
+++ ORF_regex.py
@@ -6,6 +6,9 @@ import argparse
 parser = argparse.ArgumentParser(description='A script to locate and translate all Open Reading Frames (ORFs) in a Sequence. Optional analysis of complementary strand and Markdown Output')
 parser.add_argument('-m', '--markdown',action='store_true', help="enables output of markdown files 'forward.md' (and 'reverse.md' if '-c' flag is set)")
 parser.add_argument('-c', '--complementary',action='store_true', help="complementary strand is generated and analysed")
+parser.add_argument('-t', '--tata', action= 'store_true', help="checks for presence of TATA-Box ('TATA[TA]A[TA]')")
+parser.add_argument('-d', '--distance', type=int, help="sets maximum distance of TATA-Box (counted from first T) to start of ORF (to A in ATG), defaults to 35 bp if not set, ignored when -t flag is not set")
+parser.add_argument('-k', '--kozak', action= 'store_true', help="evaluates strength of Kozak_sequence by checking the -3 bp and +4 bp site relative to the ORf's start, assumes eukariotic Sequence, DOES NOT evaluate less conserved bases in the putative Kozak-Sequence")
 parser.add_argument('file_name', help="location of file containing the Input Sequence")
 args = parser.parse_args()
 
@@ -27,7 +30,27 @@ COMPLEMENTARY=COMPLEMENTARY.replace("G","y")
 COMPLEMENTARY=COMPLEMENTARY.replace("C","G")
 COMPLEMENTARY=COMPLEMENTARY.replace("y","C")
 
-pattern=re.compile('(?=(ATG([CTGA]{3})*?(TGA|TAA|TAG)))')
+if args.tata==True:
+    print("===============================================================================================================")
+    print("NOTE:")
+    print("Presence of TATA-Box is checked via pattern 'TATA[TA]A[TA]', bases in [brackets] mean one of the listed bases")
+    print("===============================================================================================================")
+    TATA_pattern=re.compile('(?=(TATA[TA]A[TA]))')
+if args.kozak==True:
+    print("===============================================================================================================")
+    print("NOTE:")
+    print("Does not check if there is a Kozak Sequence but checks it's putative strength")
+    print("Highly conserved sites -3 bp (A or G) and + 4 bp (G) relative to the Startcodons 'A'")
+    print("Strong Kozak: both conserved bases present (-3 bp is A/G and  +4 is G)")
+    print("Medium Kozak: one of the conserved bases present (either -3 bp is A/G or +4 bp is G but not both)")
+    print("Weak Kozak: none of the conserved bases present (neither -3 bp is A/G nor +4 bp is G)")
+    print("===============================================================================================================")
+    KOZAK_strong_pattern=re.compile('(?=([ATGC]{3}[AG][ATGC]{2}ATGG))') # Kozak_sequence in which the -3 Base being A/G AND the +4 Base being G
+    KOZAK_medium_pattern=re.compile('(?=(([ATGC]{3}[TC][ATGC]{2}ATGG)|([ATGC]{3}[AG][ATGC]{2}ATG[ATC])))') #Kozak_sequence in which the -3 Base being A/G XOR the +4 Base being G (but not both)
+    KOZAK_weak_pattern=re.compile('(?=([ATGC]{3}[TC][ATGC]{2}ATG[ATC]))') #KOZAK_sequence in which the -3 base is T/C AND the +3 Base is NOT G
+
+ORF_pattern=re.compile('(?=(ATG([CTGA]{3})*?(TGA|TAA|TAG)))')
+
 CONTINUED_ORF_COUNT=0
 
 if args.complementary==True:
@@ -48,7 +71,32 @@ for SEQUENCE in (SEQUENCES_LIST):
         if args.markdown==True:
             MD_FILE=open('complementary.md', "w")
 
-    ORF_ITER=pattern.finditer(SEQUENCE)
+    if args.tata==True:
+        TATA_ITER=TATA_pattern.finditer(SEQUENCE)
+        TATA_LIST=[]
+        if args.distance== None:
+            print("no distance set, defaulting to 35 bp")
+            TATA_DISTANCE=35
+        else:
+            TATA_DISTANCE=args.distance
+            print("distance set to", TATA_DISTANCE)
+        for TOUPLE in TATA_ITER:
+            TATA_LIST.append(TOUPLE)
+    if args.kozak==True:
+        K_STRONG_ITER=KOZAK_strong_pattern.finditer(SEQUENCE)
+        K_STRONG_LIST=[]
+        for TOUPLE in K_STRONG_ITER:
+            K_STRONG_LIST.append(TOUPLE)
+        K_MEDIUM_ITER=KOZAK_medium_pattern.finditer(SEQUENCE)
+        K_MEDIUM_LIST=[]
+        for TOUPLE in K_MEDIUM_ITER:
+            K_MEDIUM_LIST.append(TOUPLE)
+        K_WEAK_ITER=KOZAK_weak_pattern.finditer(SEQUENCE)
+        K_WEAK_LIST=[]
+        for TOUPLE in K_WEAK_ITER:
+            K_WEAK_LIST.append(TOUPLE)
+
+    ORF_ITER=ORF_pattern.finditer(SEQUENCE)
     STARCOUNTER=0
     ORF_LIST=[]
     for TOUPLE in ORF_ITER:
@@ -178,6 +226,40 @@ for SEQUENCE in (SEQUENCES_LIST):
             print(' START:', len(INPUT_STRING)- ORF.start(1),'   END:',len(INPUT_STRING)- ORF.end(1), file=OUTPUT_FILE)
         print( ORF.group(1),file=OUTPUT_FILE)
         print(PEP,file=OUTPUT_FILE)
+
+        #TATA and Kozak output
+        if args.tata==True:
+            for TATA in TATA_LIST:
+                if (ORF.start(1)-TATA.start(1) <= TATA_DISTANCE) and (ORF.start(1) - TATA.start(1) >0):
+                    print("TATA-Box found within", TATA_DISTANCE, "bp", file=OUTPUT_FILE)
+                    print("TATA-Box start:", TATA.start(1), "relative to ORF:", ORF.start(1)-TATA.start(1), file=OUTPUT_FILE)
+                    if STRAND=="+":
+                        print("Sequence:", INPUT_STRING[TATA.start(1): ORF.start(1)+3],"...", file=OUTPUT_FILE)
+                    if STRAND=="-":
+                        print("Sequence:", COMPLEMENTARY[TATA.start(1): ORF.start(1)+3],"...", file=OUTPUT_FILE)
+        if args.kozak==True:
+            for K_STRONG in K_STRONG_LIST:
+                if ORF.start(1)-K_STRONG.start(1)==6:
+                    print("Strong Kozak-Sequence found (-3 bp = G/A AND +4 bp = G )", file=OUTPUT_FILE)
+                    if STRAND=="+":
+                        print("Sequence:", INPUT_STRING[K_STRONG.start(1): K_STRONG.end(1)], file=OUTPUT_FILE)
+                    if STRAND=="-":
+                        print("Sequence:", COMPLEMENTARY[K_STRONG.start(1): K_STRONG.end(1)], file=OUTPUT_FILE)
+            for K_MEDIUM in K_MEDIUM_LIST:
+                if ORF.start(1)-K_MEDIUM.start(1)==6:
+                    print("Medium strength Kozak-Sequence found (-3 bp = G/A OR +4 bp = G, but not both )", file=OUTPUT_FILE)
+                    if STRAND=="+":
+                        print("Sequence:", INPUT_STRING[K_MEDIUM.start(1): K_MEDIUM.end(1)], file=OUTPUT_FILE)
+                    if STRAND=="-":
+                        print("Sequence:", COMPLEMENTARY[K_MEDIUM.start(1): K_MEDIUM.end(1)], file=OUTPUT_FILE)
+            for K_WEAK in K_WEAK_LIST:
+                if ORF.start(1)-K_WEAK.start(1)==6:
+                    print("Weak Kozak-Sequence found (-3 bp = T/C AND +4 bp = T/C/A )", file=OUTPUT_FILE)
+                    if STRAND=="+":
+                        print("Sequence:", INPUT_STRING[K_WEAK.start(1): K_WEAK.end(1)], file=OUTPUT_FILE)
+                    if STRAND=="-":
+                        print("Sequence:", COMPLEMENTARY[K_WEAK.start(1): K_WEAK.end(1)], file=OUTPUT_FILE)
+
 if args.markdown==True:
     print(SEQUENCE, file=MD_FILE)
     MD_FILE.close()
